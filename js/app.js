8fc516b4833048060036b59517a960f4aa771b13
jucaptain <mh4877@naver.com>
2023-01-23 00:44:35 +0900

    네임드 뷰 실습

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var Body = { template: '<div>This is Body</div>' };
 var Header = { template: '<div>This is Header</div>' };
 var Footer = { template: '<div>This is Footer</div>' };
+var Login = { template: '<div>This is Login</div>' };
 
 var router = new VueRouter({
     routes : [
@@ -8,6 +9,14 @@ var router = new VueRouter({
             path: '/',
             components: {
                 default: Body,
+                header: Header,
+                footer: Footer
+            },
+        },
+        {
+            path: '/login',
+            components: {
+                default: Login,
                 header: Header,
                 footer: Footer
             },
