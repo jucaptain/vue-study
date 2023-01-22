a6e89809166c01a4c542cb70ae1177f0025ebf33
jucaptain <mh4877@naver.com>
2023-01-23 00:52:08 +0900

    axios 예제

--- js/app.js
+++ js/app.js
@@ -1,29 +1,11 @@
-var Body = { template: '<div>This is Body</div>' };
-var Header = { template: '<div>This is Header</div>' };
-var Footer = { template: '<div>This is Footer</div>' };
-var Login = { template: '<div>This is Login</div>' };
-
-var router = new VueRouter({
-    routes : [
-        {
-            path: '/',
-            components: {
-                default: Body,
-                header: Header,
-                footer: Footer
-            },
-        },
-        {
-            path: '/login',
-            components: {
-                default: Login,
-                header: Header,
-                footer: Footer
-            },
-        }
-    ]
-});
-
 var app = new Vue({
-    router
-}).$mount('#app');
+    el: '#app',
+    methods: {
+        getData: function() {
+            axios.get('https://raw.githubusercontent.com/joshua1988/doit-vuejs/master/data/demo.json')
+                .then(function (response) {
+                    console.log(response);
+                });
+        }
+    }
+});
